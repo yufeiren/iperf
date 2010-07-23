efa71a93495fe591807346d12f809ee395fe089f
Ren Yufei <renyufei83@yahoo.com.cn>
2010-07-23 17:39:32 -0400

    init rdma.h

---
+++ include/rdma.h
@@ -0,0 +1,160 @@
+/*--------------------------------------------------------------- 
+ * Copyright (c) 2010                              
+ * BNL            
+ * All Rights Reserved.                                           
+ *--------------------------------------------------------------- 
+ * Permission is hereby granted, free of charge, to any person    
+ * obtaining a copy of this software (Iperf) and associated       
+ * documentation files (the "Software"), to deal in the Software  
+ * without restriction, including without limitation the          
+ * rights to use, copy, modify, merge, publish, distribute,        
+ * sublicense, and/or sell copies of the Software, and to permit     
+ * persons to whom the Software is furnished to do
+ * so, subject to the following conditions: 
+ *
+ *     
+ * Redistributions of source code must retain the above 
+ * copyright notice, this list of conditions and 
+ * the following disclaimers. 
+ *
+ *     
+ * Redistributions in binary form must reproduce the above 
+ * copyright notice, this list of conditions and the following 
+ * disclaimers in the documentation and/or other materials 
+ * provided with the distribution. 
+ * 
+ *     
+ * Neither the names of the University of Illinois, NCSA, 
+ * nor the names of its contributors may be used to endorse 
+ * or promote products derived from this Software without
+ * specific prior written permission. 
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, 
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES 
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND 
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE CONTIBUTORS OR COPYRIGHT 
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, 
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, 
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE. 
+ * ________________________________________________________________
+ * National Laboratory for Applied Network Research 
+ * National Center for Supercomputing Applications 
+ * University of Illinois at Urbana-Champaign 
+ * http://www.ncsa.uiuc.edu
+ * ________________________________________________________________ 
+ *
+ * rdma.h
+ * by Yufei Ren <renyufei83@gmail.com>
+ * -------------------------------------------------------------------
+ * An abstract class for waiting on a condition variable. If
+ * threads are not available, this does nothing.
+ * ------------------------------------------------------------------- */
+
+#include <rdma/rdma_cma.h>
+#include <infiniband/arch.h>
+
+/*
+ * riperf data transfer type:
+ * 	1 client/server set buffer, server use RDMA READ
+ *	2 client/server set buffer, client use RDMA WRITE
+ *	3 client/server set buffer, 
+ *	  server use RDMA READ to read data from client,
+ *	  server use RDMA WRITE to write data to server.
+ *	<repeat loop>  
+ */
+
+/*
+ * These states are used to signal events between the completion handler
+ * and the main client or server thread.
+ *
+ * Once CONNECTED, they cycle through RDMA_READ_ADV, RDMA_WRITE_ADV, 
+ * and RDMA_WRITE_COMPLETE for each ping.
+ */
+enum test_state {
+	IDLE = 1,
+	CONNECT_REQUEST,
+	ADDR_RESOLVED,
+	ROUTE_RESOLVED,
+	CONNECTED,
+	RDMA_READ_ADV,
+	RDMA_READ_COMPLETE,
+	RDMA_WRITE_ADV,
+	RDMA_WRITE_COMPLETE,
+	ERROR
+};
+
+struct rping_rdma_info {
+	uint64_t buf;
+	uint32_t rkey;
+	uint32_t size;
+};
+
+/*
+ * Default max buffer size for IO...
+ */
+/* #define RPING_BUFSIZE 64*1024 */
+#define RPING_BUFSIZE 640*1024*1024
+#define RPING_SQ_DEPTH 16
+
+/* Default string for print data and
+ * minimum buffer size
+ */
+#define _stringify( _x ) # _x
+#define stringify( _x ) _stringify( _x )
+
+#define RPING_MSG_FMT           "rdma-ping-%d: "
+#define RPING_MIN_BUFSIZE       sizeof(stringify(INT_MAX)) + sizeof(RPING_MSG_FMT)
+
+/*
+ * Control block struct.
+ */
+struct rping_cb {
+	int server;			/* 0 iff client */
+	pthread_t cqthread;
+	pthread_t persistent_server_thread;
+	struct ibv_comp_channel *channel;
+	struct ibv_cq *cq;
+	struct ibv_pd *pd;
+	struct ibv_qp *qp;
+
+	struct ibv_recv_wr rq_wr;	/* recv work request record */
+	struct ibv_sge recv_sgl;	/* recv single SGE */
+	struct rping_rdma_info recv_buf;/* malloc'd buffer */
+	struct ibv_mr *recv_mr;		/* MR associated with this buffer */
+
+	struct ibv_send_wr sq_wr;	/* send work request record */
+	struct ibv_sge send_sgl;
+	struct rping_rdma_info send_buf;/* single send buf */
+	struct ibv_mr *send_mr;
+
+	struct ibv_send_wr rdma_sq_wr;	/* rdma work request record */
+	struct ibv_sge rdma_sgl;	/* rdma single SGE */
+	char *rdma_buf;			/* used as rdma sink */
+	struct ibv_mr *rdma_mr;
+
+	uint32_t remote_rkey;		/* remote guys RKEY */
+	uint64_t remote_addr;		/* remote guys TO */
+	uint32_t remote_len;		/* remote guys LEN */
+
+	char *start_buf;		/* rdma read src */
+	struct ibv_mr *start_mr;
+
+	enum test_state state;		/* used for cond/signalling */
+	sem_t sem;
+
+	struct sockaddr_storage sin;
+	uint16_t port;			/* dst port in NBO */
+	int verbose;			/* verbose logging */
+	int count;			/* ping count */
+	int size;			/* ping data size */
+	int validate;			/* validate ping data */
+
+	/* CM stuff */
+	pthread_t cmthread;
+	struct rdma_event_channel *cm_channel;
+	struct rdma_cm_id *cm_id;	/* connection on client side,*/
+					/* listener on service side. */
+	struct rdma_cm_id *child_cm_id;	/* connection on server side */
+};
+
